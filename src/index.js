6e6788a6acb260e11f55f902871ee8e2e10b3578
Aditya <adityabhatt78910@gmail.com>
2025-02-24 18:08:08 +0530

    Model created for user and videos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 import connectDB from "./db/connection.js"
+import {app} from "./app.js"
 
 dotenv.config({
     path: './env'
 })
 
+app.get('/',(req,res)=>{
+    res.send("This server is working");
+})
+
 connectDB()
+.then(()=>{
+    app.on('error',(err)=>{
+        console.log("Err:",err);
+        throw err;
+    })
+    app.listen(process.env.PORT || 4000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT || 4000}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo db connection failed",err);
+})
